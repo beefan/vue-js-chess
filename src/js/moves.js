59f7f9a7d778be88e832dbaba09da5c437f7860c
Brandon Fannin <bfannin13@gmail.com>
2020-05-03 17:45:09 -0400

    implemented rook move logic

--- src/js/moves.js
+++ src/js/moves.js
@@ -31,7 +31,6 @@ function getValidMoves (piece, state) {
 
 function getPawnMoves (state) {
   console.log('can pawn move?')
-  const turnColor = state.turn ? 'white' : 'black'
   const letter = state.selected.substring(0, 1)
   const number = Number(state.selected.substring(1))
   const diagnols = cols.filter((col, index) => {
@@ -44,7 +43,7 @@ function getPawnMoves (state) {
   let moves = 1
 
   // if the pawn is in its original place, it can move two spaces
-  if (pawnStarts[turnColor].includes(state.selected)) {
+  if (pawnStarts[getTurnColor(state)].includes(state.selected)) {
     moves = 2
   }
 
@@ -66,12 +65,73 @@ function getPawnMoves (state) {
       validMoves.push(space)
     }
   }
-
+  console.log('valid pawn moves')
+  console.log(validMoves)
+  // TODO do any of these valid moves open up the king?
   return validMoves
 }
 export function getRookMoves (state) {
   console.log('can rook move?')
-  return true
+  const letter = state.selected.substring(0, 1)
+  const number = Number(state.selected.substring(1))
+  const validMoves = []
+
+  // moves up
+  for (let i = number + 1; i <= 8; i++) {
+    const spaceId = letter + i
+    if (isSpaceEmpty(state, spaceId)) {
+      validMoves.push(spaceId)
+    } else if (getTurnColor(state) !== getPieceColor(state, spaceId)) {
+      validMoves.push(spaceId)
+      break
+    } else {
+      break
+    }
+  }
+  // moves down
+  for (let i = number - 1; i > 0; i--) {
+    const spaceId = letter + i
+    if (isSpaceEmpty(state, spaceId)) {
+      validMoves.push(spaceId)
+    } else if (getTurnColor(state) !== getPieceColor(state, spaceId)) {
+      validMoves.push(spaceId)
+      break
+    } else {
+      break
+    }
+  }
+  // moves right
+  for (let i = cols.indexOf(letter) + 1; i < cols.length; i++) {
+    const spaceId = cols[i] + number
+    console.log(' to the right ')
+    console.log(spaceId)
+    if (isSpaceEmpty(state, spaceId)) {
+      validMoves.push(spaceId)
+    } else if (getTurnColor(state) !== getPieceColor(state, spaceId)) {
+      validMoves.push(spaceId)
+      break
+    } else {
+      break
+    }
+  }
+  // moves left
+  for (let i = cols.indexOf(letter) - 1; i >= 0; i--) {
+    const spaceId = cols[i] + number
+    console.log(' to the left ')
+    console.log(spaceId)
+
+    if (isSpaceEmpty(state, spaceId)) {
+      validMoves.push(spaceId)
+    } else if (getTurnColor(state) !== getPieceColor(state, spaceId)) {
+      validMoves.push(spaceId)
+      break
+    } else {
+      break
+    }
+  }
+  console.log('valid rook moves')
+  console.log(validMoves)
+  return validMoves
 }
 export function getKnightMoves (state) {
   console.log('can knight move?')
@@ -92,4 +152,16 @@ export function getKingMoves (state) {
 
 function isSpaceEmpty (state, space) {
   return state.board.filter(x => x.id === space)[0].occupant === 'empty'
+}
+function getTurnColor (state) {
+  return state.turn ? 'white' : 'black'
+}
+
+function getPieceColor (state, space) {
+  const pieceColor = state.board
+    .filter(x => x.id === space)[0]
+    .occupant.split('-')[1]
+    .substring(0, 1)
+  console.log(pieceColor)
+  return pieceColor
 }
