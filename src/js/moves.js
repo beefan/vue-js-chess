8ee1e3552c6f026f35c23743be4a94f27cd8443b
Brandon Fannin <bfannin13@gmail.com>
2020-05-03 16:51:58 -0400

    pawn move logic implemented

--- src/js/moves.js
+++ src/js/moves.js
@@ -1,12 +1,49 @@
+const cols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+
+const pawnStarts = {
+  black: cols.map(x => x + 7),
+  white: cols.map(x => x + 2)
+}
+
 export function canPawnMove (to, state) {
   console.log('can pawn move?')
-  // const validMoves = []
-  // get all possible moves
+  const turnColor = state.turn ? 'white' : 'black'
+  const letter = state.selected.substring(0, 1)
+  const number = Number(state.selected.substring(1))
+  const diagnols = cols.filter((col, index) => {
+    return (
+      (index + 1 < cols.length && cols[index + 1] === letter) ||
+      (index - 1 > -1 && cols[index - 1] === letter)
+    )
+  })
+  const validMoves = []
+  let moves = 1
 
-  // delete any routes where someone is in the way
-  // delete any routes that put the king in check
-  // check if desired space is in that set of moves
-  return true
+  // if the pawn is in its original place, it can move two spaces
+  if (pawnStarts[turnColor].includes(state.selected)) {
+    moves = 2
+  }
+
+  // add the next spaces to validMoves if no pieces in the way
+  for (let i = 1; i <= moves; i++) {
+    if (i === 2 && validMoves.length === 0) {
+      break
+    }
+    const move = state.turn ? letter + (number + i) : letter + (number - i)
+    if (isSpaceEmpty(state, move)) {
+      validMoves.push(move)
+    }
+  }
+
+  // the pawn can take diagnol pieces
+  for (const letter of diagnols) {
+    const space = state.turn ? letter + (number + 1) : letter + (number - 1)
+    if (!isSpaceEmpty(state, space)) {
+      validMoves.push(space)
+    }
+  }
+
+  return validMoves.includes(to)
 }
 export function canRookMove (to, state) {
   console.log('can rook move?')
@@ -27,4 +64,8 @@ export function canQueenMove (to, state) {
 export function canKingMove (to, state) {
   console.log('can king move?')
   return true
+}
+
+function isSpaceEmpty (state, space) {
+  return state.board.filter(x => x.id === space)[0].occupant === 'empty'
 }
